883afe9078a7a5a5d3fcd4a20418a7930d1986d6
Walter <walterdcarrizo.19@gmail.com>
2024-12-23 20:14:42 -0300

    add orders module with create, find, and update functionalities; configure microservice transport

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('OrdersMicroservice-Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
-  await app.listen(envs.port);
-  logger.log(`Server is running on: ${envs.port}`);
+  await app.listen();
+  logger.log(`Microservice is running on: ${envs.port}`);
 }
 bootstrap();
